522f5ab09ca09f2b101bb65b84a8c8015741462e
Axzanaeb <Axzanaeb@gmail.com>
2015-12-03 10:40:54 +0000

    teste

--- app/src/main/java/com/example/axzanaeb/helloworld/IncGame.java
+++ app/src/main/java/com/example/axzanaeb/helloworld/IncGame.java
@@ -5,8 +5,8 @@ package com.example.axzanaeb.helloworld;
  */
 public class IncGame {
     int villagers = 0, farmProg, lumbProg, stoneProg, goldProg;
-    int villagers_breed_rate = 6; //      p/min
-
+    int initial_villagers_breed_rate = 6; //      p/min
+    int villagers_breed_rate = initial_villagers_breed_rate;
     public void addVillager() {
         villagers++;
     }
@@ -16,7 +16,7 @@ public class IncGame {
     }
 
     public int getVillagers_breed_rate() {
-        return villagers_breed_rate;
+        return initial_villagers_breed_rate;
     }
 
     public void setVillagersDistribution(int farmProg, int lumbProg, int stoneProg, int goldProg) {
@@ -24,5 +24,7 @@ public class IncGame {
         this.lumbProg=lumbProg;
         this.stoneProg=stoneProg;
         this.goldProg=goldProg;
+        villagers_breed_rate=initial_villagers_breed_rate*100/farmProg;
+
     }
 }
